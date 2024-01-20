801be11aadde25baf7731336e61f58d775c02602
Anonymous <anonymous@anonymous.com>
2024-01-20 15:46:48 +0800

    update

--- README.md
+++ README.md
@@ -1,6 +1,1 @@
 # Property-driven Parallel Symbolic Model Checking of LTL
-
-## usage:
-```
-cargo r --release -- -f <smv file> -p <num core>
-```
--- ppsmc/src/main.rs
+++ ppsmc/src/main.rs
@@ -15,22 +15,7 @@ type BddManager = sylvan::Sylvan;
 type Bdd = sylvan::Bdd;
 
 fn main() {
-    let input_file =
-    // "abp8-p0.smv";
-    "prod-cons-p0.smv";
-    // "production-cell.smv";
-    // "bc57-sensors-p1.smv";
-
-    // "viscoherence-p1.smv";
-    // "cunim1ro.smv";
-    // "cuhanoi7ro.smv";
-    // "cuhanoi10ro.smv";
-    // "cuabq2mfro.smv";
-    // "phils-p1.smv";
-    // "msi_wtrans.smv";
-    // "syncarb.smv";
-    // "elevator.smv";
-    // "ftp3-flat.smv"; //???
+    let input_file = "abp8-p0.smv";
 
     let mut input_file = format!("./benchmark/{}", input_file);
     let args = command::Args::parse();
